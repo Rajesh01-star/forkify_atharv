6e517dc7dc0cec6f89c9d723fc051e1a9f2ed51d
Rajesh01-star <rajeshbehera489@gmail.com>
2022-04-07 20:48:52 +0530

    fraction removed

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './view';
 
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional';
 
 class recipeView extends View {
   _parentEl = document.querySelector('.recipe');
@@ -10,8 +10,6 @@ class recipeView extends View {
 
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
-    // window.addEventListener('hashchange', handler);
-    // window.addEventListener('load', handler);
   }
 
   addHandlerUpdateServings(handler) {
@@ -104,9 +102,7 @@ class recipeView extends View {
               <svg class="recipe__icon">
                 <use href="${icons}#icon-check"></use>
               </svg>
-              <div class="recipe__quantity">${
-                ing.quantity ? new Fraction(ing.quantity).toString() : ''
-              }</div>
+              <div class="recipe__quantity">${ing.quantity}</div>
               <div class="recipe__description">
                 <span class="recipe__unit">${ing.unit}</span>
                 ${ing.description}
